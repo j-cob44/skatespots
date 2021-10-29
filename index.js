df1fb646d67a44bdc4e40bd240fb26c5935c8caf
j-cob44 <jacoburton44@gmail.com>
2021-10-29 00:36:07 -0700

    Initial Search Features
    Youtube link search

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Jacob Burton - Oct 2021
 let infowindows = new Array();
 let markerArray = new Array();
+var data;
+
+var g_map;
 
 function initMap() {
   const usa = { lat: 39.286777, lng: -101.462366 };
@@ -11,9 +14,9 @@ function initMap() {
     zoomControl: true,
   });
 
-  var filePath = './backend/pins.json';
+  registerMap(map);
 
-  var data;
+  var filePath = './backend/pins.json';
 
   $.ajax({
       async: false,
@@ -41,14 +44,25 @@ function initMap() {
       title: "#" + i,
     });
 
-    newListener(marker, infowindow)
+    newListener(marker, infowindow);
   }
 
-  /* // add something
+  /* // add pin listener
   map.addListener("dblclick", (e) => {
     placeMarkerAndPanTo(e.latLng, map);
   });
   */
+
+  // close info windows on click
+  map.addListener("click", (e) => {
+    for(let i = 0; i < infowindows.length; i++){
+      infowindows[i].close();
+    }
+  });
+}
+
+function registerMap(f_map){
+  g_map = f_map;
 }
 
 function newListener(f_marker, f_infowindow){
@@ -60,10 +74,63 @@ function newListener(f_marker, f_infowindow){
     }
     f_infowindow.open({
       anchor: f_marker,
-      map,
+      g_map,
       shouldFocus: true,
     });
   });
+}
+
+function clearMarkers(){
+  for(var i = 0; i < markerArray.length; i++){
+    markerArray[i].setMap(null);
+  }
+  markerArray = new Array();
+  infowindows = new Array();
+}
+
+// Handles User Search for Youtube Link, Skater Name, or Year
+function MarkerSearch(){
+  var user_input = document.getElementById("UserSearch").value;
+
+  if(user_input == ""){
+    //$("#UserSearch").addClass('is-invalid'); // show feedback
+    return;
+  }
+
+  clearMarkers();
+
+  var count = 0;
+  // youtube Link
+  var youtube_id = youtube_parser(user_input);
+  for(var i = 0; i < data.pins.length; i++){
+    if(data.pins[i].youtubeCode == youtube_id) {
+      console.log("here");
+      var string = CreateContentString(data.pins[i]);
+      var position = {lat: data.pins[i].lat, lng: data.pins[i].long};
+
+      infowindow = new google.maps.InfoWindow({
+        content: string,
+      });
+
+      marker = new google.maps.Marker({
+        position: position,
+        g_map,
+        title: "#" + count,
+      });
+
+      newListener(marker, infowindow);
+      count++;
+
+      marker.setMap(g_map);
+    }
+  }
+}
+
+// Youtube Parser Made by "jeffreypriebe"
+function youtube_parser(url){
+    var regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[1].length==11)? match[1] : false;
 }
 
 function CreateContentString(PinObj) {
@@ -71,9 +138,9 @@ function CreateContentString(PinObj) {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    //'<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<p class="lead">' + PinObj.youtubeTitle + '</p>' +
     '<div id="bodyContent">' +
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + PinObj.youtubeCode + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'+
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + PinObj.youtubeCode + '" frameborder="0" gesture="media" allow="autoplay; encrypted-media" allowfullscreen></iframe>'+
     '<p>Watch on Youtube: <a href="https://www.youtube.com/watch?v=' + PinObj.youtubeCode + '">' +
     "https://www.youtube.com/watch?v=" + PinObj.youtubeCode + "</a> " +
     "</p>" +
