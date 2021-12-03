22110cd6bff961564a1411abf648233c22ea0ccd
j-cob44 <jacoburton44@gmail.com>
2021-12-03 15:21:45 -0700

    Update httpPythonServer.py

--- httpPythonServer.py
+++ httpPythonServer.py
@@ -4,32 +4,24 @@ import http.server # Our http server handler for http requests
 import socketserver # Establish the TCP Socket connections
 import json
 import uuid
+import os
 from datetime import datetime
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
     def do_GET(self):
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        if (self.path == '/'):
+            self.path = '/index.html'
 
-    def do_HEAD(self):
-        self._set_headers()
+        if(os.path.dirname(self.path) == '/backend/unverified'):
+            self.send_error(404, 'File Not Found/Allowed')
+            return
 
-    # def do_POST(self):
-    #     self._set_headers()
-    #     print("in post method")
-    #     self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-    #
-    #     self.send_response(200)
-    #     self.end_headers()
-    #     print(self.data_string)
-    #     data = json.loads(self.data_string)
-    #     with open("./backend/unverified.json", "w") as outfile:
-    #         json.dump(data, outfile)
-    #     return
+        permitted_extensions = ['.html','.png','.svg','.jpg', '.js', '.json', '.css', '.ico']
+        if not os.path.splitext(self.path)[1] in permitted_extensions:
+            self.send_error(404, 'File Not Found/Allowed')
+        else:
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
+        return
 
     def do_PUT(self):
         self._set_headers()
@@ -42,19 +34,32 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
         dt = datetime.now()
         ts = datetime.timestamp(dt)
+        nowTime = dt.strftime('%Y-%m-%d-%H-%M-%S')
 
         data = {'IP': self.client_address[0], 'timestamp': str(datetime.fromtimestamp(ts, tz=None)), 'submission': submittedData}
-        dataUpURL = "./backend/unverified/" + str(uuid.uuid4().hex) + ".json"
-        with open(dataUpURL, "w") as outfile:
-            json.dump(data, outfile)
+        dataUpURL = "./backend/unverified/" + nowTime + ".json"
+        outfile = open(dataUpURL, "w")
+        json.dump(data, outfile)
+        outfile.close()
         return
 
 Handler = MyHttpRequestHandler
 
 def run(handler_class=Handler, addr="0.0.0.0", port=80):
     with socketserver.TCPServer((addr, port), handler_class) as httpd:
-        print(f"Http Server starting at {addr}:{port}")
-        httpd.serve_forever()
+        dt = datetime.now()
+        nowTime = dt.strftime('%d/%m/%Y %H:%M:%S')
+        print(f"[{nowTime}] - Http Server starting at {addr}:{port}")
+
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+
+        dt = datetime.now()
+        nowTime = dt.strftime('%d/%m/%Y %H:%M:%S')
+        print(f"[{nowTime}] - Http Server stopping at {addr}:{port}")
 
 
 if __name__ == "__main__":
